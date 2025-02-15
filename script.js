ed5dd88be28809b9a47e9629570e9f39eebd0cb5
Juliana <julianatsoc28@gmail.com>
2025-02-15 19:47:15 -0300

    feat: adiciona item na lista de solicitações

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const form = document.querySelector("form")
 const amount = document.getElementById("amount");
-const expense = document.getElementById("expensive");
+const expense = document.getElementById("expense");
 const category = document.getElementById("category");
+
+const expenseList = document.querySelector("ul");
+
 
 amount.oninput = () => {
     let value = amount.value.replace(/\D/g, "");
@@ -20,5 +23,54 @@ function formatCurrencyBRL(value) {
 
 form.onsubmit = (e) => {
     e.preventDefault();
-}
 
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date().toLocaleDateString(),
+
+    }
+    expenseAdd(newExpense);
+};
+function expenseAdd(newExpense) {
+    try {
+
+        const expenseItem = document.createElement("li");
+        expenseItem.classList.add("expense");
+
+        const expenseIcon = document.createElement("img");
+        expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+        expenseIcon.setAttribute("alt", newExpense.category_name);
+
+        const expenseInfo = document.createElement("div");
+        expenseInfo.classList.add("expense-info");
+
+        const expenseName = document.createElement("strong");
+        expenseName.textContent = newExpense.expense;
+
+        const expenseCategory = document.createElement("span");
+        expenseCategory.textContent = newExpense.category_name;
+
+
+        expenseInfo.append(expenseName, expenseCategory)
+
+        const expenseAmount = document.createElement("span");
+        expenseAmount.classList.add("expense-amount");
+        expenseAmount.innerHTML = `
+        <small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}
+
+        `;
+
+
+
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount)
+
+        expenseList.append(expenseItem)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
